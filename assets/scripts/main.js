329f4d206def4fac06cd21d65c4e453b340f0b93
Musa Shaba <musamohshaba@gmail.com>
2023-07-10 15:46:31 +0100

    Done with its interactivity on Desktop view

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+const form = document.getElementById('form2');
+const message = document.querySelector('.message p.invalid');
+
+form.addEventListener('submit', (e) => {
+  if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    message.classList.toggle('display');
+    e.preventDefault();
+  } else {
+    alert('Email Sent');
+  }
+});
