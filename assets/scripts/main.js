dd10b4b443e11e8610a8dfc53cce57eaa98b5be6
Musa Shaba <musamohshaba@gmail.com>
2023-07-11 16:25:51 +0100

    Mobile View on the way

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@ const message = document.querySelector('.message p.invalid');
 
 form.addEventListener('submit', (e) => {
   if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
-    message.classList.toggle('display');
+    message.classList.add('display');
     e.preventDefault();
   } else {
     alert('Email Sent');
