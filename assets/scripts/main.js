649569a5c2a5b4e1e6173a28b28f0d2963c56be9
Musa Shaba <musamohshaba@gmail.com>
2023-07-11 18:15:29 +0100

    Final touches and ReadMe Editing

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,9 @@ const email = document.getElementById('email');
 const form = document.getElementById('form2');
 const message = document.querySelector('.message p.invalid');
 const submit = document.querySelector("#form2 input[type='submit'");
+function theAlert() {
+  alert('Email Sent');
+}
 form.addEventListener('submit', (e) => {
   if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
     message.classList.add('display');
@@ -9,7 +12,5 @@ form.addEventListener('submit', (e) => {
     if (window.matchMedia('(max-width: 550px)').matches) {
       submit.style.marginTop = '35px';
     }
-  } else {
-    alert('Email Sent');
   }
 });
