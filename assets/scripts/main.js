144e2d11d8d0a837932f375cdb7634ca0feb5066
Musa Shaba <musamohshaba@gmail.com>
2023-07-11 17:02:10 +0100

    added the new interactivity feature:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,12 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form2');
 const message = document.querySelector('.message p.invalid');
-
+const submit = document.querySelector("#form2 input[type='submit'");
 form.addEventListener('submit', (e) => {
   if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
     message.classList.add('display');
     e.preventDefault();
+    submit.style.marginTop = '25px';
   } else {
     alert('Email Sent');
   }
